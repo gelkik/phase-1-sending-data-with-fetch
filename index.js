fca1299ef658d2aac32b979c87d4a5ad87177ebe
George Elkik <gelkik@gmail.com>
2023-01-17 20:35:39 -0500

    final

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
 // Add your code here
-
 function submitData (name, email){
-    // fetch('http://localhost:3000/users')
-    // .then(response=>response.json())
-    // .then(data=>console.log(data))
-    fetch('http://localhost:3000/users',{
+    return fetch('http://localhost:3000/users',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
         body: JSON.stringify({
-            'name': name,
-            'email': email,
+            name,email
         }),
     })
-    .then(response=>response.json())
-    .then(data=>{console.log(data)})
+    .then(response=>{
+        return response.json()
+    })
+    .then(data=>{
+        const id = document.createElement('id');
+        id.innerText = data.id;
+        document.querySelector('body').appendChild(id);
+    })
     .catch(error=>{
         alert('Error!');
         console.log(error.message)
     })
-
-
-    // const div = document.createElement('div');
-    // div.className = 'user-creation';
-
-    // const h2 = document.createElement('h2');
-    // h2.textContent = name;
-
-    // const p = document.createElement('p');
-    // p.textContent = email;
-
-    // div.append(h2,p);
-    // document.querySelector('script').append(div);
 }
 submitData('Steve','steve@steve.com')
 
