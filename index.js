4b4d127361151aa4866cd775956640803b79878f
George Elkik <gelkik@gmail.com>
2023-01-17 19:54:01 -0500

    commit tree 78c82758e88a2f5db0540f19beababa7dcedc3be

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+
+function submitData (name, email){
+    // fetch('http://localhost:3000/users')
+    // .then(response=>response.json())
+    // .then(data=>console.log(data))
+    fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email,
+        }),
+    })
+    .then(response=>response.json())
+    .then(data=>{console.log(data)})
+    .catch(error=>{
+        alert('Error!');
+        console.log(error.message)
+    })
+
+
+    // const div = document.createElement('div');
+    // div.className = 'user-creation';
+
+    // const h2 = document.createElement('h2');
+    // h2.textContent = name;
+
+    // const p = document.createElement('p');
+    // p.textContent = email;
+
+    // div.append(h2,p);
+    // document.querySelector('script').append(div);
+}
+submitData('Steve','steve@steve.com')
+
+
+
+// fetch('http://localhost:3000/users',{
+//     method: 'POST',
+//     headers: {
+//         'Content Type': 'application/json'
+//             Accept: 'application/json'
+//     }
+// })
